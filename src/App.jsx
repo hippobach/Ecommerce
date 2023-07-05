13046fc5c639bbbb0dd9aee1cdd38304fda01577
Nguyen Xuan Bach <nguyenxuanbach03nd@gmail.com>
2023-07-05 21:03:58 +0700

    feat: add library react-router-dom to route app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+// Libraries
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Pages
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+
 function App() {
-    return <div>App Component</div>;
+    return (
+        <Fragment>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </Fragment>
+    );
 }
 
 export default App;
