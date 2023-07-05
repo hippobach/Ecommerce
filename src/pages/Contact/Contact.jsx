76bbd017de99eaa39f38c1b19177df644c94a4f8
Nguyen Xuan Bach <nguyenxuanbach03nd@gmail.com>
2023-07-05 20:47:20 +0700

    feat: create main pages

---
+++ src/pages/Contact/Contact.jsx
@@ -0,0 +1,10 @@
+import styles from './Contact.module.scss';
+import classname from 'classnames/bind';
+
+const cx = classname.bind(styles);
+
+function Contact() {
+    return <div>Contact</div>;
+}
+
+export default Contact;
---
+++ src/pages/OrderHistory/OrderHistory.jsx
@@ -0,0 +1,10 @@
+import styles from './OrderHistory.module.scss';
+import classname from 'classnames/bind';
+
+const cx = classname.bind(styles);
+
+function OrderHistory() {
+    return <div>OrderHistory</div>;
+}
+
+export default OrderHistory;
