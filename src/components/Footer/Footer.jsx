0de8c81d107f284935d046712c1508ebb7060767
Nguyen Xuan Bach <nguyenxuanbach03nd@gmail.com>
2023-07-07 11:58:01 +0700

    feat: responsive header and footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,11 @@ import classname from 'classnames/bind';
 
 const cx = classname.bind(styles);
 
+const date = new Date();
+const year = date.getFullYear();
+
 function Footer() {
-    return <div className={cx('footer')}>Footer</div>;
+    return <div className={cx('footer')}>&copy; {year} All Rights Reserved</div>;
 }
 
 export default Footer;
